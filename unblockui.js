770e9317218017cc8c7fcecc649634204238f6f6
Areth Kellejian <arethk@gmail.com>
2016-06-09 14:57:28 -0700

    adding some comments

--- unblockui.js
+++ unblockui.js
@@ -1,5 +1,6 @@
-// get the black blocking ui div
-var blockUIDiv = document.getElementById("wsopContentGatingContainer").remove();
+// get the black blocking ui div and delete it
+document.getElementById("wsopContentGatingContainer").remove();
 
 // set the overflow of body to auto
 document.body.style.overflow = "auto";
+
