367b1a5b0c4f089485575be8d76f3f57c8784569
Areth Kellejian <arethk@gmail.com>
2016-06-09 14:56:17 -0700

    simplifying code

--- unblockui.js
+++ unblockui.js
@@ -1,8 +1,5 @@
 // get the black blocking ui div
-var blockUIDiv = document.getElementById("wsopContentGatingContainer");
-
-// delete the black blocking ui div
-blockUIDiv.remove();
+var blockUIDiv = document.getElementById("wsopContentGatingContainer").remove();
 
 // set the overflow of body to auto
 document.body.style.overflow = "auto";
